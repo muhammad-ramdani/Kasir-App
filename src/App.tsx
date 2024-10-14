57855312a2e2b07069fe480b05d2b47239d36cb1
Muhammad Ramdani <muhammadramdanibjrs@gmail.com>
2024-10-14 21:08:17 +0700

    membuat slicing manajemen (Data Barang/Produk)

--- src/App.tsx
+++ src/App.tsx
@@ -14,41 +14,43 @@ import LaporanPelanggan from "./Pages/Laporan/LaporanPelanggan";
 import LaporanPersediaanBarang from "./Pages/Laporan/LaporanPersediaaanBarang";
 import Pengaturan from "./Pages/Pengaturan/Pengaturan";
 import InformasiToko from "./Pages/Pengaturan/InformasiToko";
-import Transaksi from "./Pages/transaksi/transaksi";
-import Pembayaran from "./Pages/transaksi/pembayaran";
-import StrukPage from "./Pages/transaksi/struck";
-import Shift from "./Pages/shift/shift";
+// import Transaksi from "./Pages/transaksi/transaksi";
+// import Pembayaran from "./Pages/transaksi/pembayaran";
+// import StrukPage from "./Pages/transaksi/struck";
+// import Shift from "./Pages/shift/shift";
+import DataBarang from "./Pages/manajemen/dataBarang";
 
 function App() {
-  return (
+    return (
 
-    <>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/change-password" element={<ChangePassword />} />
-        <Route path="/verify-email" element={<VerifyEmail />} />
-        <Route path="/new-passwd" element={<NewPasswd />} />
-        <Route path="/reset-berhasil" element={<ResetBerhasil />} />
-        <Route path="/register" element={<Register />} />
-        {/* Pages laporan */}
-        <Route path="/laporan" element={<Laporan />} />
-        <Route path="/laporan-transaksi" element={<LaporanTransaksi />} />
-        <Route path="/laporan-pembelian-barang" element={<LaporanPembelianBarang />} />
-        <Route path="/laporan-pelanggan" element={<LaporanPelanggan />} />
-        <Route path="/laporan-persediaan-barang" element={<LaporanPersediaanBarang />} />
-        {/* Pages Transaksi */}
-        <Route path="/transaksi" element={<Transaksi />} />
-        <Route path="/transaksi/pembayaran" element={<Pembayaran />} />
-        <Route path="/transaksi/struk" element={<StrukPage />} />
-        {/* Pages Shift */}
-        <Route path="/shift" element={<Shift />} />
-        {/* Pengaturan */}
-        <Route path="/pengaturan" element={<Pengaturan />} />
-        <Route path="/informasi-toko" element={<InformasiToko />} />
-      </Routes>
-    </>
-  );
+        <>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/change-password" element={<ChangePassword />} />
+                <Route path="/verify-email" element={<VerifyEmail />} />
+                <Route path="/new-passwd" element={<NewPasswd />} />
+                <Route path="/reset-berhasil" element={<ResetBerhasil />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/databarang" element={<DataBarang />} />
+                {/* Pages laporan */}
+                <Route path="/laporan" element={<Laporan />} />
+                <Route path="/laporan-transaksi" element={<LaporanTransaksi />} />
+                <Route path="/laporan-pembelian-barang" element={<LaporanPembelianBarang />} />
+                <Route path="/laporan-pelanggan" element={<LaporanPelanggan />} />
+                <Route path="/laporan-persediaan-barang" element={<LaporanPersediaanBarang />} />
+                {/* Pages Transaksi */}
+                {/* <Route path="/transaksi" element={<Transaksi />} />
+                <Route path="/transaksi/pembayaran" element={<Pembayaran />} />
+                <Route path="/transaksi/struk" element={<StrukPage />} /> */}
+                {/* Pages Shift */}
+                {/* <Route path="/shift" element={<Shift />} /> */}
+                {/* Pengaturan */}
+                <Route path="/pengaturan" element={<Pengaturan />} />
+                <Route path="/informasi-toko" element={<InformasiToko />} />
+            </Routes>
+        </>
+    );
 }
 
 
